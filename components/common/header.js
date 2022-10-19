3ebab6cc7ef6d34e6b20efd7b024fb5ed56d0ba5
Sulaimaan <mailsulaimaan@gmail.com>
2022-10-19 21:37:33 +0530

    header and banner changes

--- components/common/header.js
+++ components/common/header.js
@@ -12,7 +12,8 @@ const Header = () => {
      
       <div class="d-flex flex-column flex-md-row align-items-center pb-2 mb-4 border p-2 border-opacity-10 rounded-pill shadow-sm bg-white">
           <a href="index.html" class="d-flex align-items-center text-dark text-decoration-none">
-            <Image width="150px" class="ps-5 myLogo" alt="Logo" src="/image/logo.png"/> <span class="pt-4 text-success"><b>Academy</b></span>
+            <Image width="100" height="100" class="ps-5 myLogo" alt="Logo" src="/image/logo.png"/> 
+            <span class="pt-4 text-success"><b>Academy</b></span>
           </a>
 
           <p class="pt-3 ms-md-auto pe-4 text-success">
