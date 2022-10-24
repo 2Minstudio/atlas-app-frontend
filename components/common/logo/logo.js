c6b260adaa33e06a1c2d8180a2040f6fa7b20c01
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2022-10-24 21:09:50 +0530

    remove comp css

--- components/common/logo/logo.js
+++ components/common/logo/logo.js
@@ -2,7 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo(props) {
-  const { align } = props;
+  const { align, hideText = true } = props;
+  const subText = hideText ? (
+    <span className="pt-4 text-success">
+      <b>Academy</b>
+    </span>
+  ) : (
+    ""
+  );
   return (
     <>
       <Link href={"/"}>
@@ -10,10 +17,7 @@ export default function Logo(props) {
           <span className={align ? "left" : "ps-5"}>
             <Image width="120" height="65" alt="Logo" src="/image/logo.png" />
           </span>
-
-          <span className="pt-4 text-success">
-            <b>Academy</b>
-          </span>
+          {subText}
         </a>
       </Link>
     </>
