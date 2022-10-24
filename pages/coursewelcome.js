b44fed67e20aabc41222dbce9429eea245f53aa6
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2022-10-24 18:28:46 +0530

    layour file name

--- pages/coursewelcome.js
+++ pages/coursewelcome.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Banner from "../components/common/banner";
-import LayoutC from "../components/common/LayoutUser";
+import LayoutC from "../components/common/layoutUser";
 import styles from "../styles/Home.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
