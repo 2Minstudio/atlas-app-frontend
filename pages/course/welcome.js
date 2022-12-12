14b5700e65783c82adcc294c9d27d537c4038b2e
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2022-12-13 00:45:15 +0530

    fix build error for link

--- pages/course/welcome.js
+++ pages/course/welcome.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import LayoutUser from "../../components/layout/layoutUser";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
