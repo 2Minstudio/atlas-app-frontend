f2e9a874a3ec40b35b871a8d5eebeca70c0b77d2
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2023-01-11 02:11:13 +0530

    adding sessions to admin pages

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,10 +1,31 @@
 import React from "react";
+import Router, { withRouter } from "next/router";
+import { withCookies } from "react-cookie";
 import LayoutAdminDashboard from "../../../components/layout/layoutAdminDashboard";
+import { isClientLoggedin, getUser } from "../../../helpers/helper";
 
 class Users extends React.Component {
+  state = {};
+  async componentDidMount() {
+    const token = isClientLoggedin(this.props);
+    if (token) {
+      const {
+        state,
+        data: { user },
+      } = await getUser(token);
+      if (state) {
+        this.setState({ user });
+      }
+    } else {
+      Router.push("/");
+    }
+  }
   render() {
-    return <LayoutAdminDashboard>Users</LayoutAdminDashboard>;
+    const { user } = this.state;
+    return (
+      <LayoutAdminDashboard user={user}>Coming soon Users</LayoutAdminDashboard>
+    );
   }
 }
 
-export default Users;
+export default withCookies(withRouter(Users));
