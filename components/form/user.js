5b7484933a9365f1d9d11d27842087925139b2d8
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2023-01-16 00:05:42 +0530

    fix spelling mistake

--- components/form/user.js
+++ components/form/user.js
@@ -141,7 +141,7 @@ class UserForm extends React.Component {
           >
             <option>Select</option>
             {roles.map((role) => {
-              return <option key={rold.id} value={role.id}>{role.name}</option>;
+              return <option key={role.id} value={role.id}>{role.name}</option>;
             })}
           </Form.Select>
           {errors?.groups && <Alert variant={"danger"}>{errors.groups}</Alert>}
