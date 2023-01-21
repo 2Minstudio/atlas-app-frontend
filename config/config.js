5cfc6271e453a42dd538035dcfdc8b2650c0acc7
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2023-01-21 18:30:08 +0530

    redirect path based on role

--- config/config.js
+++ config/config.js
@@ -2,4 +2,8 @@ export const config = {
   siteTitle: "Atlas Chiropractic & Wellness",
   siteDescription:
     "Atlas Chiropractic & Wellness, a one-stop holistic healing and wellness centre, offering result-oriented healing services from Chiropractic to Mental Health and many more. Call us today or request an appointment online",
+  redirectByRole: {
+    3: "/course/welcome",
+    4: "/admin",
+  },
 };
