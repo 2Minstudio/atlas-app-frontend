8d72365df4d9e8194dc7f3f27f5636743394c0b9
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2023-01-28 01:07:09 +0530

    default open

--- components/course/modulePreviewList.js
+++ components/course/modulePreviewList.js
@@ -1,21 +1,24 @@
-import Image from "next/image";
 import Accordion from "react-bootstrap/Accordion";
 function ModuleList({ mode, data }) {
+  let i = 0;
   return (
     <>
       <h5 className="pt-4">Lessons</h5>
       <div className="col col-md-6 col-lg-7">
-        <Accordion defaultActiveKey="0">
-          {data?.map((module) => (
-            <Accordion.Item key={module.id} eventKey="0">
-              <Accordion.Header>{module.name}</Accordion.Header>
-              <Accordion.Body>
-                {module?.chapters.map((chapter) => (
-                  <p>{chapter.name}</p>
-                ))}
-              </Accordion.Body>
-            </Accordion.Item>
-          ))}
+        <Accordion defaultActiveKey="module-1">
+          {data?.map((module) => {
+            i++;
+            return (
+              <Accordion.Item key={`module-${i}`} eventKey={`module-${i}`}>
+                <Accordion.Header>{module.name}</Accordion.Header>
+                <Accordion.Body>
+                  {module?.chapters.map((chapter) => (
+                    <p key={`chapter-${chapter.id}`}>{chapter.name}</p>
+                  ))}
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
         </Accordion>
       </div>
     </>
