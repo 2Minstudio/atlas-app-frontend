e32686ce03751551233a3c2c6ba07166d8f4c138
Abdul Nazurudeen <abdul.nazurudeen@gmail.com>
2023-03-03 22:29:04 +0530

    hide home

--- components/breadcrumb/admin.js
+++ components/breadcrumb/admin.js
@@ -3,7 +3,7 @@ import Breadcrumb from "react-bootstrap/Breadcrumb";
 function AdminBreadcrumb({ items }) {
   return (
     <Breadcrumb>
-      <Breadcrumb.Item href="/admin/">Home</Breadcrumb.Item>
+      {/* <Breadcrumb.Item href="/admin/">Home</Breadcrumb.Item> */}
       {Object.keys(items).map((k) => {
         return (
           <Breadcrumb.Item href={k} key={k}>
